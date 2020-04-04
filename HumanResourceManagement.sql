cdace9d061dcc692ac5ebdda9843441448609846
dolemta9899 <61912750+dolemta9899@users.noreply.github.com>
2020-04-04 22:49:43 +0700

    Tạo database ban đầu và các bảng dữ liệu
    mới chỉ có tạo bảng thôi nha mọi người

---
+++ HumanResourceManagement.sql
@@ -0,0 +1,33 @@
+create database HumanResourceManagement
+go
+use HumanResourceManagement
+go
+
+
+create table TaiKhoan(
+acc char(15) not null primary key,
+pass char(15) not null
+)
+go
+create table PhongBan(
+	[maPB] [char](10) NOT NULL,
+	[tenPB] [nvarchar](30) NOT NULL,
+	[SDT] [char](10) NULL,
+	[Email] [char](30) NULL,
+	
+);
+go
+
+create table NhanVien(
+	[maNV] [char](10) NOT NULL,
+	[tenNV] [nvarchar](30) NOT NULL,
+	[ngaySinh] [date] NULL,
+	[gioiTinh] [nchar](5) NULL,
+	[diaChi] [nvarchar](50) NULL,
+	[SDT] [char](13) NULL,
+	[maPB] [char](10) NOT NULL,
+	[Luong] [money] NULL,
+);
+
+
+
