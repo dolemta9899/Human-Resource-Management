7184026fc52ad807c90e184607313fa9d1db374b
dolemta9899 <61912750+dolemta9899@users.noreply.github.com>
2020-04-07 22:49:12 +0700

    file BUS(hoàn thiện giao diện)
    đây là file hoàn thiện nhé

---
+++ BUS/BUS.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using QLNS.DataAccessLayer;
+using QLNS.ValueObject;
+
+namespace QLNS.BUSLayer
+{
+    public class BUS
+    {
+      
+        public static int them_TK(TaiKhoan a)
+        {
+            return DAO.them_TK(a);
+        }
+        //phong ban
+
+    }
+
+
+}
+
