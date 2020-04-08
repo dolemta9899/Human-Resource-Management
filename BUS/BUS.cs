1c080bfcaa3e035617d9b5572b67f6838c4f816d
LanNguyen28 <61912463+LanNguyen28@users.noreply.github.com>
2020-04-08 23:34:39 +0700

    bổ sung in ra bảng và thêm modul thêm nv vs pb
    modul thêm nhân viên và phòng ban

--- BUS/BUS.cs
+++ BUS/BUS.cs
@@ -17,6 +17,23 @@ namespace QLNS.BUSLayer
             return DAO.them_TK(a);
         }
         //phong ban
+        public static int them_PB(PhongBan a)
+        {
+            return DAO.them_PB(a);
+        }
+        //nhan vien
+        public static int them_NV(NhanVien a)
+        {
+            return DAO.them_NV(a);
+        }
+        public static DataTable xuat_NV()
+        {
+            return DAO.xuat_NV();
+        }
+        public static DataTable xuat_PB()
+        {
+            return DAO.xuat_PB();
+        }
 
     }
 
