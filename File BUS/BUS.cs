97d97574e97358664d05c7142a8a01f95f7af8d8
tranhuyen123 <59789766+tranhuyen123@users.noreply.github.com>
2020-04-09 23:14:22 +0700

    Bổ sung phần sửa nhé!!
    mình chỉ up đoạn code thôi nhé, nhờ leader add vào ;))

---
+++ File BUS/BUS.cs
@@ -0,0 +1,10 @@
+﻿
+        public static int sua_PB(PhongBan a)
+        {
+            return DAO.sua_PB(a);
+        }
+        public static int sua_NV(NhanVien a)
+        {
+            return DAO.sua_NV(a);
+        }
+   
